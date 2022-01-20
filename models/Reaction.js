ba4ba0fe7643e83cc457d37383c51d1fe9ec4b9e
bravotango <seattleriot@hotmail.com>
2022-01-20 15:52:53 -0800

    Added models

---
+++ models/Reaction.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const reactionSchema = new mongoose.Schema({
+  reactionId: { type: mongoose.Schema.Types.ObjectId },
+  reactionBody: { type: String, required: true, maxlength: 280 },
+  username: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Reaction = mongoose.model('Reaction', reactionSchema);
+
+module.exports = { reactionSchema, Reaction };
