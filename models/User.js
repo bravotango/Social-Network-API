03d645f21d0c4e963883995d84745bbbe1088dfd
bravotango <seattleriot@hotmail.com>
2022-01-27 19:03:36 -0800

    Thoughts are working and assigning to user

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
-
 const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true, trim: true },
@@ -11,7 +10,7 @@ const userSchema = new mongoose.Schema(
       validate: [isEmail, 'invalid email'],
     },
     // Array of _id values referencing the Thought model
-    thoughts: { type: [Number], required: false, unique: true, trim: true },
+    thoughts: [{ type: mongoose.Schema.ObjectId, ref: 'Thought' }],
     // Array of _id values referencing the User model (self-reference)
     friends: { type: [Number], required: false, unique: true, trim: true },
   },
@@ -28,15 +27,5 @@ userSchema.virtual('friendCount').get(function () {
 });
 
 const User = mongoose.model('User', userSchema);
-
-// const handleError = (err) => console.error(err);
-
-// User.create(
-//   {
-//     username: 'bt',
-//     email: 'bt@home.com',
-//   },
-//   (err) => (err ? handleError(err) : console.log('Created new document'))
-// );
 
 module.exports = User;
