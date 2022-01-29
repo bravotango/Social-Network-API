1eb32a433acd8bfd2326837645349f3b6bbeed9f
bravotango <seattleriot@hotmail.com>
2022-01-29 15:47:35 -0800

    Reactions are working

--- models/Thought.js
+++ models/Thought.js
@@ -6,7 +6,7 @@ const thoughtSchema = new mongoose.Schema(
     thoughtText: { type: String, required: true, minlength: 1, maxlength: 280 },
     createdAt: { type: Date, default: Date.now, required: true },
     username: { type: String, required: true },
-    reactions: [reactionSchema],
+    reactions: [{ type: mongoose.Schema.ObjectId, ref: 'Reaction' }],
   },
   {
     toJSON: {
