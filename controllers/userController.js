7c37234b6b4582daea96b98579b0952323bf50ee
bravotango <seattleriot@hotmail.com>
2022-01-30 15:05:30 -0800

    Delete user & all user's thoughts working

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,17 @@
-const User = require('../models/User');
+const user = require('../models/User');
+const thought = require('../models/Thought');
 
 module.exports = {
   getUsers(req, res, next) {
-    User.find()
+    user
+      .find()
       .populate('thoughts')
       .then((users) => res.json(users))
       .catch((err) => next(err));
   },
   getSingleUser(req, res, next) {
-    User.findOne({ _id: req.params.userId })
+    user
+      .findOne({ _id: req.params.userId })
       .populate('thoughts')
       .then((user) =>
         !user
@@ -18,19 +21,25 @@ module.exports = {
       .catch((err) => next(err));
   },
   createUser(req, res, next) {
-    User.create(req.body)
+    user
+      .create(req.body)
       .then((dbUserData) => res.status(200).json(dbUserData))
       .catch((err) => next(err));
   },
   editUser(req, res, next) {
-    User.findOneAndUpdate({ _id: req.params.userId }, req.body, {
-      returnDocument: 'after',
-    })
+    user
+      .findOneAndUpdate({ _id: req.params.userId }, req.body, {
+        returnDocument: 'after',
+      })
       .then((dbUpdatedUser) => res.status(200).json(dbUpdatedUser))
       .catch((err) => next(err));
   },
   deleteUser(req, res, next) {
-    User.deleteOne({ _id: req.params.userId })
+    user.findOne({ _id: req.params.userId }).then((foundUser) => {
+      return thought.deleteMany({ username: foundUser.username });
+    });
+    user
+      .deleteOne({ _id: req.params.userId })
       .then((dbDeleteResult) =>
         !dbDeleteResult
           ? res.status(404).json({ message: 'No user with that id' })
@@ -39,22 +48,24 @@ module.exports = {
       .catch((err) => next(err));
   },
   addFriend(req, res, next) {
-    User.findOneAndUpdate(
-      { _id: req.params.userId },
-      { $addToSet: { friends: req.params.friendId } },
-      { returnDocument: 'after' }
-    )
+    user
+      .findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.params.friendId } },
+        { returnDocument: 'after' }
+      )
       .then((dbUpdatedUser) => {
         res.status(200).json(dbUpdatedUser);
       })
       .catch((err) => next(err));
   },
   deleteFriend(req, res, next) {
-    User.findOneAndUpdate(
-      { _id: req.params.userId },
-      { $pull: { friends: req.params.friendId } },
-      { returnDocument: 'after' }
-    )
+    user
+      .findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friends: req.params.friendId } },
+        { returnDocument: 'after' }
+      )
       .then((dbUpdatedUser) => {
         res.status(200).json(dbUpdatedUser);
       })
